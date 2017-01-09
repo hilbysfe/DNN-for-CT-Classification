deff2328c8194a2f0435faccd79319b3423b7a7d
unknown <ahilbert@13-013892.amc.intra>
2017-01-09 17:55:22 +0100

    [++] Loading labels added to utils [==] Seperate skullstripping and organized notebooks a bit

---
+++ Utils/skullStripping2D.py
@@ -0,0 +1,124 @@
+
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
+# ============ Skull stripping with 2D connected component segmentation ==============
+
+def skullstripping(path):
+
+#	rootDir = 'D:\\AdamHilbert\\DNN_Classification_Project\\data\\'
+#	datasetDir = 'MRCLEAN_CT24h'
+#	patient = '\\0001\\CT24h\\thick\\'
+#	fileName = 'pat0001.mhd'
+
+#	path = rootDir + datasetDir + patient + fileName
+
+	# --- Read Image ---
+	image = sitk.ReadImage(path)
+
+	# --- Load Data ---
+
+	#center = 30
+	#width = 80
+
+	#window_filter = sitk.IntensityWindowingImageFilter()
+	#window_filter.SetWindowMinimum(center - (width / 2))
+	#window_filter.SetWindowMaximum(center + (width / 2))
+	#window_filter.SetOutputMinimum(0)
+	#window_filter.SetOutputMaximum(255)
+
+	img_data = sitk.GetArrayFromImage(image)
+
+	#img_plot = sitk.GetArrayFromImage(window_filter.Execute(img))
+
+	data_new = np.zeros((30,512,512))
+
+	for k in range(30):
+		# --- Thresholding ---
+		img = img_data[k,:,:]
+		
+		for i in range(3,509):
+			for j in range(3,509):
+				if img[i,j] < 80 and img[i,j] > 0 \
+					and np.min(img[i-3:i+3, j-3:j+3]) < 0 and np.max(img[i-3:i+3, j-3:j+3]) > 80:
+					img[i,j] = 0
+
+		g = img < 70
+		#print(g)
+
+		# --- Find connected component of brain ---
+
+		w = np.zeros((512,512))
+
+		dx = [0,0,1,1,1,-1,-1,-1]
+		dy = [1,-1,0,1,-1,0,1,-1]
+
+
+		def dfs_it(x, y, c):
+			visited, stack = set(), [(x,y)]
+			while stack:
+				v = stack.pop()
+				if v not in visited:
+					visited.add(v)
+					w[v[0],v[1]] = c
+					for i in range(8):
+						# for more sophisticated method here could be a check for surrounding average intensity
+						nx = v[0]+dx[i]
+						ny = v[1]+dy[i]
+						if nx >= 0 and ny >= 0 and nx < 512 and ny < 512 \
+							and g[nx,ny] and (nx,ny) not in visited and img[nx,ny] > 0:
+							stack.append((nx,ny))
+
+
+		s = 1
+		for i in range(0,512):
+			for j in range(0,512):
+				if g[i,j] == True and w[i,j] == 0 and img[i,j] > 10:
+					dfs_it(i,j,s)
+					s = s+1
+
+
+	#    print(s)
+	#    plt.imshow(w)
+
+		# --- Calculate mean intensity of connected components ---
+		mean_int = []
+		img_components = []
+		component_sizes = []
+		for i in range(s):
+			ind_i = np.where(w == i)
+			component_sizes.append(np.shape(ind_i)[1])
+			img_compi = np.zeros((512,512))
+			for j in range(np.shape(ind_i)[1]):
+				x = ind_i[0][j]
+				y = ind_i[1][j]
+				img_compi[x][y] = img[x][y]
+			mean_int.append(np.true_divide(img_compi.sum(),(img_compi!=0).sum()))
+			img_components.append(img_compi)
+
+		# --- Select valid components ---
+		valid_means = [i for i in mean_int if i <= 50 and i >= 10]
+		valid_indices = [mean_int.index(i) for i in valid_means]
+		valid_sizes = [component_sizes[i] for i in valid_indices]
+	#    print(valid_means)
+	#    print(valid_sizes)
+
+		data_stripped = np.zeros((512,512))
+		for i in valid_indices:
+			if component_sizes[i] > 5000:
+				data_stripped = data_stripped + img_components[i]
+				
+		data_new[k,:,:] = data_stripped
+	#    print(k)
+		
+#	img_new = sitk.GetImageFromArray(data_new)
+
+	# --- Save image ---
+#	newFileName = fileName.split(".")[0] + '_skullstripped.mhd'
+
+#	sitk.WriteImage(img_new, rootDir + datasetDir + patient + newFileName)
+
+	return data_new
--- Utils/utils.py
+++ Utils/utils.py
@@ -3,111 +3,127 @@ from __future__ import division
 import os
 import SimpleITK as sitk
 import numpy as np
+import openpyxl as ox
 
-def read_datasets(images_root_path, subfolder_name, label_path, validation_ratio):
-	"""
-	Function to read up images and labels.
-	Store only paths as images would fit to memory.
-	"""
-	patients = [name for name in os.listdir(images_root_path)
-            if os.path.isdir(os.path.join(images_root_path, name))]
-	# subfolder_name = "\\CT24h\\thick\\"
-	
-	all_samples = len(patients)
-	
-	all_data = []
-	labels = np.zeros(all_samples)
-	
-	for name in patients:
-		sample_filepath = images_root_path + subfolder_name + "pat" + name.zfill(4) + ".mhd"
-		all_data.append(filepath)
-		
-	with open(label_path, 'rb') as f:
-		label_list = f.readlines()
-	
-	for i in range(all_samples):
-		labels[i] = int(label_list[i])
+def read_datasets(data_dir, images_root_sub, image_sub, label_root_sub, label_filename, validation_ratio):
+    """
+    Function to read up images and labels.
+    Store only paths as images would fit to memory.
 
-	perm = np.arange(all_samples)
-	np.random.shuffle(perm)
-	
-	validation_size = all_samples*validation_ratio
-	
-	training_images = np.empty(shape=[all_samples-validation_size], dtype=string)
-	training_labels = np.zeros(all_samples-validation_size)
-	validation_images = np.empty(shape=[validation_size], dtype=string)
-	validation_labels = np.zeros(validation_size)
-	
-	for i in range(all_samples):
-		if i < validation_size:
-			validation_images[i] = all_data[perm[i]]
-			validation_labels[i] = labels[perm[i]]
-		else:
-			training_images[i] = all_data[perm[i]]
-			training_labels[i] = labels[perm[i]]
-	
-	return DataSet(training_images, training_labels), DataSet(validation_images, validation_labels)
-		
-		
+    MRS@90 hardcoded as label attribute in label_filename -> column 'AE'
+    """
+    
+    followid_attribute = 'AB2:AB501'
+    label_attribute = 'AE2:AE501'
+    
+    # --- Retrieve all patients we have images from ---
+    patients = [name for name in os.listdir(data_dir + images_root_sub)
+            if os.path.isdir(os.path.join(data_dir + images_root_sub, name))]
+    # image_sub = "CT24h\\thick\\"
+    all_samples = len(patients)
+
+    # --- Store image paths ---
+    all_images = [data_dir + images_root_sub + name.zfill(4) + '\\' + image_sub + 'pat' + name.zfill(4) + '.mhd' \
+                    for name in patients ]
+    
+    # --- Load labels from file ---
+    labels_wb = ox.load_workbook(data_dir + label_root_sub + label_filename)
+    labels_ws = labels_wb.active
+
+    label_dict = {key[0].value.zfill(4): value[0].value 
+                  for (key, value) in zip(labels_ws[followid_attribute], labels_ws[label_attribute]) }
+
+    labels = np.array( [ label_dict[name] for name in patients] )
+
+    # --- Shuffle example to split fairly ---	
+    perm = np.arange(all_samples)
+    np.random.shuffle(perm)
+
+    # rounding instead?
+    validation_size = int(all_samples*validation_ratio)
+    
+    # --- Split data to training and validation set ---
+    training_images = []
+    training_labels = []
+    validation_images = []
+    validation_labels = []
+
+    for i in range(all_samples):
+        if i < validation_size:
+            validation_images.append(all_images[perm[i]])
+            validation_labels.append(labels[perm[i]])
+        else:
+            training_images.append(all_images[perm[i]])
+            training_labels.append(labels[perm[i]])
+
+    # --- Return 2 DataSet objects for training and validation set ---	
+    return DataSet(np.array(training_images), np.array(training_labels)), \
+            DataSet(np.array(validation_images), np.array(validation_labels))
+
+
 
 class DataSet(object):
-	"""
-	Utility class to handle dataset structure.
-	"""
+    """
+    Utility class to handle dataset structure.
+    """
 
-	def __init__(self, images, labels):
-		"""
-		Builds dataset with images and labels.
-		Args:
-			images: Images data.
-			labels: Labels data
-		"""
-		assert images.shape[0] == labels.shape[0], (
-					"images.shape: {0}, labels.shape: {1}".format(str(images.shape), str(labels.shape)))
+    def __init__(self, images, labels):
+        """
+        Builds dataset with images and labels.
+        Args:
+            images: Images data.
+            labels: Labels data
+        """
+        assert images.shape[0] == labels.shape[0], (
+                    "images.shape: {0}, labels.shape: {1}".format(str(images.shape), str(labels.shape)))
 
-		self._num_examples = images.shape[0]
-		self._images = images
-		self._labels = labels
-		self._epochs_completed = 0
-		self._index_in_epoch = 0
+        self._num_examples = images.shape[0]
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
 
-	@property
-	def labels(self):
-		return self._labels
+    @property
+    def images(self):
+        return self._images
 
-	@property
-	def num_examples(self):
-		return self._num_examples
+    @property
+    def labels(self):
+        return self._labels
 
-	@property
-	def epochs_completed(self):
-		return self._epochs_completed
+    @property
+    def num_examples(self):
+        return self._num_examples
 
-	def next_batch(self, batch_size):
-		"""
-		Return the next `batch_size` examples from this data set.
-		Args:
-			batch_size: Batch size.
-		"""
-		start = self._index_in_epoch
-		self._index_in_epoch += batch_size
-		if self._index_in_epoch > self._num_examples:
-			self._epochs_completed += 1
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
 
-			perm = np.arange(self._num_examples)
-			np.random.shuffle(perm)
-			self._images = self._images[perm]
-			self._labels = self._labels[perm]
+    def next_batch(self, batch_size):
+        """
+        Return the next `batch_size` examples from this data set.
+        Args:
+            batch_size: Batch size.
+        """
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+            self._epochs_completed += 1
 
-			start = 0
-			self._index_in_epoch = batch_size
-			assert batch_size <= self._num_examples
+            perm = np.arange(self._num_examples)
+            np.random.shuffle(perm)
+            self._images = self._images[perm]
+            self._labels = self._labels[perm]
 
-		end = self._index_in_epoch
-		
-		image_batch = np.array([ \
-			sitk.GetArrayFromImage(sitk.ReadImage(image_path)) \
-				for image_path in self._images[start:end]])
-		label_batch = self._labels[start:end]
-		
-		return image_batch, label_batch
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+
+        end = self._index_in_epoch
+
+        image_batch = np.array([ \
+            sitk.GetArrayFromImage(sitk.ReadImage(image_path)) \
+                for image_path in self._images[start:end]])
+        label_batch = self._labels[start:end]
+
+        return image_batch, label_batch
