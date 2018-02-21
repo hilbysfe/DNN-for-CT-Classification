[!!] Conv3d locally fixed [++] Added full derivatives basis set + structured steerable set [++] Changed couple of things in training alg

Training/Models/densenet3d.py
@@ -5,7 +5,7 @@ from Utils.cnn_utils import _conv_layer_pure_3d
 
 TF_VERSION = float('.'.join(tf.__version__.split('.')[:2]))
 
-class DenseNet(object):
+class DenseNet3d(object):
 	def __init__(self, growth_rate, depth,
 				 total_blocks, keep_prob,
 				 model_type,
@@ -92,11 +92,11 @@ class DenseNet(object):
 		"""
 		with tf.variable_scope("composite_function"):
 			# BN
-			output = self.batch_norm(_input)
+#			output = self.batch_norm(_input)
 			# ReLU
-			output = tf.nn.relu(output)
+			output = tf.nn.relu(_input)
 			# convolution
-			output = _conv_layer_pure_3d(output, shape=[kernel_size, kernel_size, kernel_size, int(output.get_shape()[-1]), out_features])
+			output, weights = _conv_layer_pure_3d(output, shape=[kernel_size, kernel_size, kernel_size, int(output.get_shape()[-1]), out_features])
 			# dropout(in case of training and in case it is no 1.0)
 			output = self.dropout(output)
 		return output
@@ -104,14 +104,14 @@ class DenseNet(object):
 	def bottleneck(self, _input, out_features):
 		with tf.variable_scope("bottleneck"):
 			# BN
-			output = self.batch_norm(_input)
+#			output = self.batch_norm(_input)
 			# ReLU
-			output = tf.nn.relu(output)
+			output = tf.nn.relu(_input)
 			inter_features = out_features * 4
 			# 1x1 convolution
 			output, weights = _conv_layer_pure_3d(output, shape=[1, 1, 1, int(output.get_shape()[-1]), inter_features], padding='VALID')
-			self.bc_conv_act.append(output)
-			self.bc_weights.append(weights)
+#			self.bc_conv_act.append(output)
+#			self.bc_weights.append(weights)
 		output = self.dropout(output)
 		return output
 
@@ -129,9 +129,9 @@ class DenseNet(object):
 				bottleneck_out, out_features=growth_rate, kernel_size=self.comp_kernel)
 		# concatenate _input with out from composite function
 		if TF_VERSION >= 1.0:
-			output = tf.concat(axis=3, values=(_input, comp_out))
+			output = tf.concat(axis=4, values=(_input, comp_out))
 		else:
-			output = tf.concat(3, (_input, comp_out))
+			output = tf.concat(4, (_input, comp_out))
 		return output
 
 	def add_block(self, _input, growth_rate, layers_per_block):
@@ -162,15 +162,15 @@ class DenseNet(object):
 		- FC layer multiplication
 		"""
 		# BN
-		output = self.batch_norm(_input)
+#		output = self.batch_norm(_input)
 		# ReLU
-		output = tf.nn.relu(output)
+		output = tf.nn.relu(_input)
 		# average pooling
 		last_pool_kernel = [1, int(output.get_shape()[1].value), int(output.get_shape()[2]) * self.avgpool_kernel_ratio, int(output.get_shape()[3].value), 1]
 		last_pool_stride = [1, int(output.get_shape()[1].value), int(output.get_shape()[2]) * self.avgpool_stride_ratio, int(output.get_shape()[3].value), 1]
-		output = tf.nn.avg_pool(output, last_pool_kernel, last_pool_stride, 'VALID')
+		output = tf.nn.avg_pool3d(output, last_pool_kernel, last_pool_stride, 'VALID')
 		# FC
-		features_total = int(output.get_shape()[-1]) * int(output.get_shape()[-2])
+		features_total = int(output.get_shape()[-1]) * int(output.get_shape()[-2]) * int(output.get_shape()[-3])
 		output = tf.reshape(output, [-1, features_total])
 		W = self.weight_variable_xavier(
 			[features_total, self.n_classes], name='W')
@@ -224,10 +224,11 @@ class DenseNet(object):
 		layers_per_block = self.layers_per_block
 		# first - initial 3 x 3 conv to first_output_features
 		with tf.variable_scope("Initial_convolution"):
-			output = _conv_layer_pure_3d(
+			output, weights = _conv_layer_pure_3d(
 				X,
 				shape=[self.initial_kernel, self.initial_kernel, self.initial_kernel, int(X.get_shape()[-1]), self.first_output_features],
 				strides=[1,2,2,2,1])
+		output = tf.nn.max_pool3d(output, ksize=[1,3,3,3,1], strides=[1,2,2,2,1], padding='SAME')
 
 		# add N required blocks
 		for block in range(self.total_blocks):

Training/Utils/utils.py
@@ -9,8 +9,10 @@ import pickle
 import tensorflow as tf
 import re
 
+#MIN_BOUND = 0.0
+#MAX_BOUND = 255.0
 MIN_BOUND = 0.0
-MAX_BOUND = 255.0
+MAX_BOUND = 100.0
 
 
 def _add_loss_summaries(total_loss):
@@ -54,6 +56,15 @@ def online_flattened_mean(files):
 	for file in files:
 		data = normalize_image(sitk.GetArrayFromImage(sitk.ReadImage(file)))
 		mean += (np.sum(data) / (len(files) * np.shape(data)[0] * np.shape(data)[1]))
+
+	return mean
+
+def online_flattened_mean_3d(files):
+	# Calculates mean of all pixels in the dataset, considering the intensities normalized
+	mean = 0
+	for file in files:
+		data = normalize_image(sitk.GetArrayFromImage(sitk.ReadImage(file)))
+		mean += (np.sum(data) / (len(files) * np.shape(data)[0] * np.shape(data)[1] * np.shape(data)[2]))
 
 	return mean
 
@@ -115,7 +126,7 @@ def read_dataset(datapath, labelpath, test_ratio=0.0):
 
 	# --- Load labels from file ---
 	labels_wb = ox.load_workbook(labelpath)
-	labels_ws = labels_wb['Registry_affected_sides']
+	labels_ws = labels_wb['Registrydatabase']
 
 #	label_dict = {key[0].value: value[0].value
 #				  for i, (key, value) in enumerate(zip(labels_ws[followid_attribute], labels_ws[label_attribute]))
@@ -233,8 +244,9 @@ def split_dataset_NCCT_CTA(datapath_NCCT, datapath_CTA, output_path):
 
 class DataSet(object):
 
-	def __init__(self, training_points, test_points, cross_validation_folds=0, normalize=False):
+	def __init__(self, training_points, test_points, cross_validation_folds=0, normalize=False, img3d=False):
 		print('Init Dataset...')
+		self.img3d = img3d
 		# === TEST-SET ===
 		# --- Split and shuffle ---
 		perm = np.arange(int(len(test_points) / 2))
@@ -280,7 +292,10 @@ class DataSet(object):
 				self._Test.Normalization = True
 
 				print('Computing mean...')
-				mean = online_flattened_mean(self._Training.images)
+				if not self.img3d:
+					mean = online_flattened_mean(self._Training.images)
+				else:
+					mean = online_flattened_mean_3d(self._Training.images)
 				print('Computing mean...done.')
 
 				self._Training.setNormalizationParameters(mean)
@@ -343,7 +358,10 @@ class DataSet(object):
 
 			if normalize:
 				print('Computing mean...')
-				mean = online_flattened_mean(self._Training.images)
+				if not self.img3d:
+					mean = online_flattened_mean(self._Training.images)
+				else:
+					mean = online_flattened_mean_3d(self._Training.images)
 				print('Computing mean...done.')
 
 				self.Normalization = True
@@ -379,7 +397,10 @@ class DataSet(object):
 
 		if self.Normalization:
 			print('Computing mean and std image...')
-			mean = online_flattened_mean(self._Training.images)
+			if not self.img3d:
+				mean = online_flattened_mean(self._Training.images)
+			else:
+				mean = online_flattened_mean_3d(self._Training.images)
 			print('Computing mean and std image...done.')
 
 			self._Training.Normalization = True
@@ -568,12 +589,11 @@ class SubSet(object):
 		)[perm]
 		#        image_batch = np.array([ np.zeros((3,512,512)) if t[0]==0 else np.ones((3,512,512)) for t in label_batch])
 
-#		image_batch = np.swapaxes(image_batch, 1, 2)
-#		image_batch = np.swapaxes(image_batch, 2, 3)
-
 		# --- Only in case of 3D model ---
 		if bases3d:
 			image_batch = np.expand_dims(image_batch, axis=4)
+			image_batch = np.swapaxes(image_batch, 1, 2)
+			image_batch = np.swapaxes(image_batch, 2, 3)
 		else:
 			image_batch = np.expand_dims(image_batch, axis=3)
 
@@ -590,7 +610,6 @@ class SubSet(object):
 			return normalize_image(sitk.GetArrayFromImage(sitk.ReadImage(image_path))) - self._mean
 		else:
 			sl = normalize_image(sitk.GetArrayFromImage(sitk.ReadImage(image_path)))
-#			sl = np.expand_dims(sl, axis=0)
 			return sl
 
 class SubSetCifar(object):

Training/train_model.py
@@ -3,6 +3,7 @@ from Utils import utils
 from Models.rfnn import RFNN
 from Models.ctnet import CTNET
 from Models.densenet import DenseNet
+from Models.densenet3d import DenseNet3d
 from Models.RFNN_densenet import RFNNDenseNet
 from Models.ae import Autoencoder
 
@@ -33,7 +34,7 @@ def train_ctnet(FLAGS, NUM_GPUS):
 
 			dataset = utils.DataSet(training_points, test_points,
 									cross_validation_folds=FLAGS.xvalidation_folds,
-									normalize=FLAGS.normalization)
+									normalize=FLAGS.normalization, img3d=FLAGS.bases3d)
 			print('Loading Dataset...done.')
 
 			# ====== DEFINE SPACEHOLDERS ======
@@ -119,9 +120,9 @@ def train_ctnet(FLAGS, NUM_GPUS):
 			print('Defining necessary OPs...')
 
 			#            opt = tf.train.GradientDescentOptimizer(FLAGS.learning_rate)
-#			opt = tf.train.MomentumOptimizer(
-#				FLAGS.learning_rate, FLAGS.nesterov_momentum, use_nesterov=True)
-			opt = tf.train.AdamOptimizer(FLAGS.learning_rate, beta1=FLAGS.beta1, beta2=FLAGS.beta2, epsilon=FLAGS.epsilon)
+			opt = tf.train.MomentumOptimizer(
+				FLAGS.learning_rate, FLAGS.nesterov_momentum, use_nesterov=True)
+#			opt = tf.train.AdamOptimizer(FLAGS.learning_rate, beta1=FLAGS.beta1, beta2=FLAGS.beta2, epsilon=FLAGS.epsilon)
 #			opt = tf.contrib.opt.NadamOptimizer(FLAGS.learning_rate, epsilon=FLAGS.epsilon)
 
 			# === DEFINE QUEUE OPS ===
@@ -224,12 +225,11 @@ def train_ctnet(FLAGS, NUM_GPUS):
 			train_op = opt.apply_gradients(avg_grads, global_step=global_step)
 
 			# Track the moving averages of all trainable variables.
-			#            variable_averages = tf.train.ExponentialMovingAverage(
-			#                0.9999, global_step)
-			#            variables_averages_op = variable_averages.apply(tf.trainable_variables())
+#			variable_averages = tf.train.ExponentialMovingAverage(0.9999, global_step)
+#			variables_averages_op = variable_averages.apply(tf.trainable_variables())
 
 			# Group all updates into a single train op.
-			#            train_op = tf.group(apply_gradient_op, variables_averages_op)
+#			train_op = tf.group(apply_gradient_op, variables_averages_op)
 
 			print('Defining update OPs...done.')
 
@@ -292,33 +292,44 @@ def train_ctnet(FLAGS, NUM_GPUS):
 					test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/test/' + str(f))
 
 					# ======== LSUV INIT WEIGHTS WITH A FORWARD PASS ==========
-#					print("Initializing weights with LSUV...")
-#					# Conv layers
-#					for l in range(len(model.alphas)):
-#						var = 0.0
-#						t_i = 0
-#						while abs(var - 1.0) >= FLAGS.tol_var and t_i < FLAGS.t_max:
-#							sess.run(batch_enqueue, feed_dict=feed_dict(0))
-#							alphas, b_l = sess.run([model.alphas[l], model.conv_act[l]], feed_dict={is_training: False})
-#							var = np.var(b_l)
-#							#							print(var)
-#							sess.run(model.alphas[l].assign(alphas / np.sqrt(var)))
-#							t_i += 1
+					print("Initializing weights with LSUV...")
+					# Dequeue data
+					xs = []
+					for i in range(FLAGS.t_max):
+						sess.run(batch_enqueue, feed_dict=feed_dict(0))
+						batch = sess.run(x, feed_dict=feed_dict(0))
+						xs.append(batch)
+					# Conv layers
+					for l in range(len(model.alphas)):
+						var = 0.0
+						t_i = 0
+						while (abs(var - 1.0) >= FLAGS.tol_var or t_i <= 5) and t_i < FLAGS.t_max:
+							alphas, b_l = sess.run([model.alphas[l], model.conv_act[l]], feed_dict={x: xs[t_i], is_training: False})
+							var = np.var(b_l)
+							sess.run(model.alphas[l].assign(alphas / np.sqrt(var)))
+							t_i += 1
+					for l in range(len(model.weights)):
+						var = 0.0
+						t_i = 0
+						while (abs(var - 1.0) >= FLAGS.tol_var or t_i <= 5) and t_i < FLAGS.t_max:
+							weights, b_l = sess.run([model.weights[l], model.fl_act[l]], feed_dict={x: xs[t_i], is_training: False})
+							var = np.var(b_l)
+							sess.run(model.weights[l].assign(weights / np.sqrt(var)))
+							t_i += 1
 					# Bottleneck layers
-#					if FLAGS.bc_mode:
-#						print("bottleneck")
-#						for l in range(len(model.bc_conv_act)):
-#							var = 0.0
-#							t_i = 0
-#							while abs(var - 1.0) >= FLAGS.tol_var and t_i < FLAGS.t_max:
-#								sess.run(batch_enqueue, feed_dict=feed_dict(0))
-#								w_l, b_l = sess.run([model.bc_weights[l], model.bc_conv_act[l]],
-#													feed_dict={is_training: False})
-#								var = np.var(b_l)
-#								#							print(var)
-#								sess.run(model.bc_weights[l].assign(w_l / np.sqrt(var)))
-#								t_i += 1
-#					print("Initializing weights with LSUV...done.")
+					if FLAGS.bc_mode:
+						print("bottleneck")
+						for l in range(len(model.bc_conv_act)):
+							var = 0.0
+							t_i = 0
+							while (abs(var - 1.0) >= FLAGS.tol_var or t_i <= 5) and t_i < FLAGS.t_max:
+								w_l, b_l = sess.run([model.bc_weights[l], model.bc_conv_act[l]],
+													feed_dict={x: xs[t_i], is_training: False})
+								var = np.var(b_l)
+								#							print(var)
+								sess.run(model.bc_weights[l].assign(w_l / np.sqrt(var)))
+								t_i += 1
+					print("Initializing weights with LSUV...done.")
 
 					max_acc = 0
 					for i in range(FLAGS.max_epochs):
@@ -330,7 +341,7 @@ def train_ctnet(FLAGS, NUM_GPUS):
 						if i == FLAGS.reduce_lr_epoch_1:
 							lr = lr / 10
 							if FLAGS.bnorm_inc:
-								bnorm_mom = bnorm_mom * 1.0714
+								bnorm_mom = bnorm_mom * 1.1429
 						if i == FLAGS.reduce_lr_epoch_2:
 							lr = lr / 10
 							if FLAGS.bnorm_inc:
@@ -408,7 +419,7 @@ def train_ctnet(FLAGS, NUM_GPUS):
 
 							if tot_acc > max_acc:
 								max_acc = tot_acc
-							print('Validation loss at step %s: %s' % (i, tot_loss))
+							print('Validation loss-acc at step %s: %s - %s' % (i, tot_loss, tot_acc))
 
 					# if i % FLAGS.checkpoint_freq == 0: # or i == FLAGS.max_steps:
 					# checkpoint_path = os.path.join(FLAGS.checkpoint_dir, 'model.ckpt')
@@ -444,6 +455,9 @@ def train_ctnet(FLAGS, NUM_GPUS):
 							FLAGS.learning_rate/100.0)
 
 					avg_train_writer.add_summary(summary, i)
+
+				avg_train_writer.close()
+				avg_test_writer.close()
 			except Exception as e:
 				# Report exceptions to the coordinator.
 				coord.request_stop(e)