[!!] fixed multiscale normalization [==] couple arrangements for running parallel experiments

Training/Models/RFNN_densenet.py
@@ -21,6 +21,7 @@ from Utils.rfnn_utils import _rfnn_conv_layer_pure_2d_SO_avg
 from Utils.rfnn_utils import _rfnn_conv_layer_pure_2d_SO_max
 from Utils.rfnn_utils import _rfnn_conv_layer_pure_2d_SO_learn_fl_bc
 from Utils.rfnn_utils import _rfnn_conv_layer_pure_2d_SO_learn_sq_bc
+from Utils.rfnn_utils import _rfnn_conv_layer_pure_2d_SO_learn_flatten
 
 from Utils.cnn_utils import _conv_layer_pure_2d
 
@@ -88,13 +89,14 @@ class RFNNDenseNet(object):
 		self.hermit_initial = init_basis_hermite_steerable_2D(self.initial_kernel, self.init_sigmas, theta=self.thetas[0], order=init_order) \
 			if rfnn is not "single" else init_basis_hermite_2D(self.initial_kernel, self.init_sigmas[0], init_order)
 		self.hermit_composite = init_basis_hermite_steerable_2D(self.comp_kernel, self.comp_sigmas, theta=self.thetas[1], order=comp_order) \
-			if rfnn is not "single" else init_basis_hermite_2D(self.comp_kernel, self.comp_sigmas[1], comp_order)
+			if rfnn is not "single" else init_basis_hermite_2D(self.comp_kernel, self.comp_sigmas[0], comp_order)
 
 		self.rfnn_layer = \
 			_rfnn_conv_layer_pure_2d_SO_learn_fl_bc if rfnn=="learn_fl" else \
 			_rfnn_conv_layer_pure_2d_SO_learn_sq_bc if rfnn=="learn_sq" else \
 			_rfnn_conv_layer_pure_2d_SO_avg if rfnn=="avg" else \
 			_rfnn_conv_layer_pure_2d_SO_max if rfnn=="max" else \
+			_rfnn_conv_layer_pure_2d_SO_learn_flatten if rfnn=="flatten" else \
 			_rfnn_conv_layer_pure_2d
 
 		if not bc_mode:
@@ -120,19 +122,10 @@ class RFNNDenseNet(object):
 			for dim in shape:
 				variable_parametes *= dim.value
 			total_parameters += variable_parametes
-		print("Total training params: %.1fM" % (total_parameters / 1e6))
-
-	def log_loss_accuracy(self, loss, accuracy, epoch, prefix, should_print=True):
-		if should_print:
-			print("mean cross_entropy: %f, mean accuracy: %f" % (
-				loss, accuracy))
-		summary = tf.Summary(value=[
-			tf.Summary.Value(
-				tag='loss_%s' % prefix, simple_value=float(loss)),
-			tf.Summary.Value(
-				tag='accuracy_%s' % prefix, simple_value=float(accuracy))
-		])
-		self.summary_writer.add_summary(summary, epoch)
+		if total_parameters / 1e6 < 1:
+			print("Total training params: %.1fK" % (total_parameters / 1e3))
+		else:
+			print("Total training params: %.1fM" % (total_parameters / 1e6))
 
 	def composite_function(self, _input, out_features, kernel_size=3):
 		"""Function from paper H_l that performs:
@@ -152,7 +145,7 @@ class RFNNDenseNet(object):
 #				self.bc_weights.append(weights)
 #				self.bc_conv_act.append(output)
 			else:
-				output, alphas = self.rfnn_layer(output, self.hermit_composite, out_features,
+				output, alphas, _ = self.rfnn_layer(output, self.hermit_composite, out_features,
 												 self.is_training, self.bnorm_momentum, self.renorm)
 #				self.kernels.append(kernel)
 				self.alphas.append(alphas)
@@ -290,10 +283,10 @@ class RFNNDenseNet(object):
 		layers_per_block = self.layers_per_block
 		# first - initial 3 x 3 conv to first_output_features
 		with tf.variable_scope("Initial_convolution"):
-			output, alphas = self.rfnn_layer(X, self.hermit_initial, self.first_output_features,
+			output, alphas, kernel = self.rfnn_layer(X, self.hermit_initial, self.first_output_features,
 											 self.is_training, self.bnorm_momentum, self.renorm, strides=[1,2,2,1])
 			print(output.get_shape())
-#			self.kernels.append(kernel)
+			self.kernels.append(kernel)
 			self.alphas.append(alphas)
 			self.conv_act.append(output)
 		with tf.variable_scope("Initial_pooling"):
@@ -311,5 +304,287 @@ class RFNNDenseNet(object):
 
 		with tf.variable_scope("Transition_to_classes"):
 			logits = self.transition_layer_to_classes(output)
+
+		self._count_trainable_params()
+
+		return logits
+
+
+class RFNNDenseNetCifar(object):
+	def __init__(self, growth_rate, depth,
+				 total_blocks, keep_prob,
+				 model_type,
+				 is_training,
+				 init_kernel,
+				 comp_kernel,
+				 init_sigmas,
+				 comp_sigmas,
+				 init_order,
+				 comp_order,
+				 thetas,
+				 rfnn,
+				 bnorm_momentum,
+				 renorm=0.7,
+				 reduction=1.0,
+				 bc_mode=False,
+				 beta_wd=0.0,
+				 avgpool_kernel_ratio=1.0,
+				 avgpool_stride_ratio=1.0,
+				 n_classes=10):
+
+		self.kernels = []
+		self.alphas = []
+		self.conv_act = []
+#		self.bc_conv_act = []
+#		self.bc_weights = []
+		self.weights = []
+		self.fl_act = []
+		self.bnorm_momentum = bnorm_momentum
+		self.renorm = renorm
+		self.beta_wd = beta_wd
+
+		self.n_classes = n_classes
+		self.depth = depth
+		self.growth_rate = growth_rate
+		self.bc_mode = bc_mode
+		self.avgpool_kernel_ratio = avgpool_kernel_ratio
+		self.avgpool_stride_ratio = avgpool_stride_ratio
+
+		# how many features will be received after first convolution
+		# value the same as in the original Torch code
+		if self.bc_mode:
+			self.first_output_features = growth_rate * 2
+		else:
+			self.first_output_features = 16
+		self.total_blocks = total_blocks
+		self.layers_per_block = (depth - (total_blocks + 1)) // total_blocks
+
+		# compression rate at the transition layers
+		self.reduction = reduction
+		self.is_training = is_training
+
+		self.initial_kernel = init_kernel
+		self.comp_kernel = comp_kernel
+		self.init_sigmas = init_sigmas
+		self.comp_sigmas = comp_sigmas
+		self.thetas = thetas
+
+		self.hermit_initial = init_basis_hermite_steerable_2D(self.initial_kernel, self.init_sigmas, theta=self.thetas[0], order=init_order) \
+			if rfnn is not "single" else init_basis_hermite_2D(self.initial_kernel, self.init_sigmas[0], init_order)
+		self.hermit_composite = init_basis_hermite_steerable_2D(self.comp_kernel, self.comp_sigmas, theta=self.thetas[1], order=comp_order) \
+			if rfnn is not "single" else init_basis_hermite_2D(self.comp_kernel, self.comp_sigmas[0], comp_order)
+
+		self.rfnn_layer = \
+			_rfnn_conv_layer_pure_2d_SO_learn_fl_bc if rfnn=="learn_fl" else \
+			_rfnn_conv_layer_pure_2d_SO_learn_sq_bc if rfnn=="learn_sq" else \
+			_rfnn_conv_layer_pure_2d_SO_avg if rfnn=="avg" else \
+			_rfnn_conv_layer_pure_2d_SO_max if rfnn=="max" else \
+			_rfnn_conv_layer_pure_2d_SO_learn_flatten if rfnn=="flatten" else \
+			_rfnn_conv_layer_pure_2d
+
+		if not bc_mode:
+			print("Build %s model with %d blocks, "
+				  "%d composite layers each." % (
+					  model_type, self.total_blocks, self.layers_per_block))
+		if bc_mode:
+			self.layers_per_block = self.layers_per_block // 2
+			print("Build %s model with %d blocks, "
+				  "%d bottleneck layers and %d composite layers each." % (
+					  model_type, self.total_blocks, self.layers_per_block,
+					  self.layers_per_block))
+		print("Reduction at transition layers: %.1f" % self.reduction)
+
+		self.keep_prob = keep_prob
+		self.model_type = model_type
+
+	def _count_trainable_params(self):
+		total_parameters = 0
+		for variable in tf.trainable_variables():
+			shape = variable.get_shape()
+			variable_parametes = 1
+			for dim in shape:
+				variable_parametes *= dim.value
+			total_parameters += variable_parametes
+		if total_parameters / 1e6 < 1:
+			print("Total training params: %.1fK" % (total_parameters / 1e3))
+		else:
+			print("Total training params: %.1fM" % (total_parameters / 1e6))
+
+	def composite_function(self, _input, out_features, kernel_size=3):
+		"""Function from paper H_l that performs:
+		- batch normalization
+		- ReLU nonlinearity
+		- convolution with required kernel
+		- dropout, if required
+		"""
+		with tf.variable_scope("composite_function"):
+			# BN
+			output = self.batch_norm(_input)
+			# ReLU
+			output = tf.nn.relu(output)
+			# convolution
+			if kernel_size == 1:
+				output, weights = _conv_layer_pure_2d(output, shape=[1, 1, output.get_shape()[-1].value, out_features], padding='VALID')
+#				self.bc_weights.append(weights)
+#				self.bc_conv_act.append(output)
+			else:
+				output, alphas, _ = self.rfnn_layer(output, self.hermit_composite, out_features,
+												 self.is_training, self.bnorm_momentum, self.renorm)
+#				self.kernels.append(kernel)
+				self.alphas.append(alphas)
+				self.conv_act.append(output)
+			# dropout(in case of training and in case it is no 1.0)
+			output = self.dropout(output)
+		return output
+
+	def bottleneck(self, _input, out_features):
+		with tf.variable_scope("bottleneck"):
+			# BN
+			output = self.batch_norm(_input)
+			# ReLU
+			output = tf.nn.relu(output)
+			inter_features = out_features * 4
+			# 1x1 convolution
+			output, weights = _conv_layer_pure_2d(output, shape=[1, 1, output.get_shape()[-1].value, inter_features], padding='VALID')
+#			self.bc_conv_act.append(output)
+#			self.bc_weights.append(weights)
+		output = self.dropout(output)
+		return output
+
+	def add_internal_layer(self, _input, growth_rate):
+		"""Perform H_l composite function for the layer and after concatenate
+		input with output from composite function.
+		"""
+		# call composite function with 3x3 kernel
+		if not self.bc_mode:
+			comp_out = self.composite_function(
+				_input, out_features=growth_rate, kernel_size=self.comp_kernel)
+		elif self.bc_mode:
+			bottleneck_out = self.bottleneck(_input, out_features=growth_rate)
+			comp_out = self.composite_function(
+				bottleneck_out, out_features=growth_rate, kernel_size=self.comp_kernel)
+		# concatenate _input with out from composite function
+		if TF_VERSION >= 1.0:
+			output = tf.concat(axis=3, values=(_input, comp_out))
+		else:
+			output = tf.concat(3, (_input, comp_out))
+		return output
+
+	def add_block(self, _input, growth_rate, layers_per_block):
+		"""Add N H_l internal layers"""
+		output = _input
+		for layer in range(layers_per_block):
+			with tf.variable_scope("layer_%d" % layer):
+				output = self.add_internal_layer(output, growth_rate)
+		print(output.get_shape())
+		return output
+
+	def transition_layer(self, _input):
+		"""Call H_l composite function with 1x1 kernel and after average
+		pooling
+		"""
+		# call composite function with 1x1 kernel
+		out_features = int(int(_input.get_shape()[-1]) * self.reduction)
+		output = self.composite_function(
+			_input, out_features=out_features, kernel_size=1)
+		print(output.get_shape())
+		# run average pooling
+		output = self.avg_pool(output, k=2, s=2)
+		print(output.get_shape())
+		return output
+
+	def transition_layer_to_classes(self, _input):
+		"""This is last transition to get probabilities by classes. It perform:
+		- batch normalization
+		- ReLU nonlinearity
+		- wide average pooling
+		- FC layer multiplication
+		"""
+		# BN
+		output = self.batch_norm(_input)
+		# ReLU
+		output = tf.nn.relu(output)
+		# average pooling
+		last_pool_kernel = [1, int(output.get_shape()[1].value), int(output.get_shape()[2]) * self.avgpool_kernel_ratio, 1]
+		last_pool_stride = [1, int(output.get_shape()[1].value), int(output.get_shape()[2]) * self.avgpool_stride_ratio, 1]
+		output = tf.nn.avg_pool(output, last_pool_kernel, last_pool_stride, 'VALID')
+		print(output.get_shape())
+		# FC
+		features_total = int(output.get_shape()[-1])*int(output.get_shape()[-2])
+		output = tf.reshape(output, [-1, features_total])
+		W = self.weight_variable_xavier(
+			[features_total, self.n_classes], name='W')
+		self.weights.append(W)
+		bias = self.bias_variable([self.n_classes])
+		logits = tf.matmul(output, W) + bias
+		self.fl_act.append(logits)
+
+		return logits
+
+	def avg_pool(self, _input, k, s):
+		ksize = [1, k, k, 1]
+		strides = [1, s, s, 1]
+		padding = 'VALID'
+		output = tf.nn.avg_pool(_input, ksize, strides, padding)
+		return output
+
+	def batch_norm(self, _input):
+		output = tf.contrib.layers.batch_norm(
+			_input, decay=self.bnorm_momentum, is_training=self.is_training, center=False,
+			renorm=True, renorm_decay=self.renorm)#, param_regularizers={'beta': tf.contrib.layers.l2_regularizer(self.beta_wd)})
+		return output
+
+	def dropout(self, _input):
+		if self.keep_prob < 1:
+			output = tf.cond(
+				self.is_training,
+				lambda: tf.nn.dropout(_input, self.keep_prob),
+				lambda: _input
+			)
+		else:
+			output = _input
+		return output
+
+	def weight_variable_msra(self, shape, name):
+		return tf.get_variable(
+			name=name,
+			shape=shape,
+			initializer=tf.contrib.layers.variance_scaling_initializer())
+
+	def weight_variable_xavier(self, shape, name):
+		return tf.get_variable(
+			name,
+			shape=shape,
+			initializer=tf.contrib.layers.xavier_initializer())
+
+	def bias_variable(self, shape, name='bias'):
+		initial = tf.constant(0.0, shape=shape)
+		return tf.get_variable(name, initializer=initial)
+
+	def inference(self, X):
+		growth_rate = self.growth_rate
+		layers_per_block = self.layers_per_block
+		# first - initial 3 x 3 conv to first_output_features
+		with tf.variable_scope("Initial_convolution"):
+			output, alphas, kernel = self.rfnn_layer(X, self.hermit_initial, self.first_output_features,
+											 self.is_training, self.bnorm_momentum, self.renorm, strides=[1,1,1,1])
+			print(output.get_shape())
+			self.kernels.append(kernel)
+			self.alphas.append(alphas)
+			self.conv_act.append(output)
+
+		# add N required blocks
+		for block in range(self.total_blocks):
+			with tf.variable_scope("Block_%d" % block):
+				output = self.add_block(output, growth_rate, layers_per_block)
+			# last block exist without transition layer
+			if block != self.total_blocks - 1:
+				with tf.variable_scope("Transition_after_block_%d" % block):
+					output = self.transition_layer(output)
+
+		with tf.variable_scope("Transition_to_classes"):
+			logits = self.transition_layer_to_classes(output)
+
+		self._count_trainable_params()
 
 		return logits

Training/Utils/rfnn_utils.py
@@ -77,7 +77,7 @@ def _rfnn_conv_layer_2d(input, basis, omaps, strides, padding, is_training, bnor
 	return alphas, outputs, kernels
 
 
-def _rfnn_conv_layer_pure_2d(input, basis, omaps, strides=[1, 1, 1, 1], padding='SAME'):
+def _rfnn_conv_layer_pure_2d(input, basis, omaps, is_training, bn_mom, rn_mom, strides=[1, 1, 1, 1], padding='SAME'):
 	with tf.device('/cpu:0'):
 		alphas = tf.get_variable(
 			'alphas',
@@ -88,7 +88,6 @@ def _rfnn_conv_layer_pure_2d(input, basis, omaps, strides=[1, 1, 1, 1], padding=
 		tf.transpose(alphas[:, :, :, None, None]) *
 		tf.transpose(basis[None, None, :, :, :])
 		, axis=2, name='weights')
-
 	output = tf.nn.conv2d(input, kernel, strides=strides, padding=padding)
 
 	#		with tf.variable_scope('sigma%d' % i):
@@ -228,7 +227,7 @@ def _rfnn_conv_layer_pure_2d_scales_max(input, basis, omaps, strides=[1, 1, 1, 1
 	outputs = tf.stack(outputs)
 	output = tf.reduce_max(outputs, reduction_indices=[0])
 
-	return output, alphas
+	return output, alphas, kernels
 
 def _rfnn_conv_layer_pure_2d_scales_avg(input, basis, omaps, strides=[1, 1, 1, 1], padding='SAME'):
 	with tf.device('/cpu:0'):
@@ -256,7 +255,7 @@ def _rfnn_conv_layer_pure_2d_scales_avg(input, basis, omaps, strides=[1, 1, 1, 1
 	outputs = tf.stack(outputs)
 	output = tf.reduce_mean(outputs, reduction_indices=[0])
 
-	return output, alphas
+	return output, alphas, kernels
 
 def _rfnn_conv_layer_pure_2d_scales_learn(input, basis, omaps, strides=[1, 1, 1, 1], padding='SAME'):
 	with tf.device('/cpu:0'):
@@ -303,7 +302,7 @@ def _rfnn_conv_layer_pure_2d_scales_learn(input, basis, omaps, strides=[1, 1, 1,
 	#
 	#			tf.summary.image('filters', kernel_transposed, max_outputs=batch)
 
-	return output, alphas
+	return output, alphas, kernels
 
 def _rfnn_conv_layer_pure_2d_scales_learn_bc(input, basis, omaps, is_training, strides=[1, 1, 1, 1], padding='SAME'):
 	with tf.device('/cpu:0'):
@@ -405,7 +404,7 @@ def _rfnn_conv_layer_pure_2d_SO_max(input, basis, omaps, is_training, bn_mom, rn
 	scales = np.shape(basis)[0]
 	orientations = np.shape(basis)[1]
 
-#	kernels = []
+	kernels = []
 	outputs = []
 	for i in range(scales):
 		for j in range(orientations):
@@ -416,7 +415,7 @@ def _rfnn_conv_layer_pure_2d_SO_max(input, basis, omaps, is_training, bn_mom, rn
 			kernel = kernel[0,0,:,:,:,:]
 			conv = tf.nn.conv2d(input, kernel, strides=strides, padding=padding)
 
-#			kernels.append(kernel)
+			kernels.append(kernel)
 			outputs.append(conv)
 
 	outputs = tf.stack(outputs)
@@ -424,7 +423,13 @@ def _rfnn_conv_layer_pure_2d_SO_max(input, basis, omaps, is_training, bn_mom, rn
 	output = tf.reduce_max(outputs, reduction_indices=[0])
 #	kernel = tf.reduce_max(kernels, reduction_indices=[0])
 
-	return output, alphas
+	kernels = tf.stack(kernels)
+	kernels = tf.transpose(kernels, (1,2,3,4,0))
+	kernels = tf.reshape(kernels, (kernels.get_shape()[0].value,kernels.get_shape()[1].value,
+								   kernels.get_shape()[2].value,
+								   kernels.get_shape()[3].value*kernels.get_shape()[4].value))
+
+	return output, alphas, kernels
 
 def _rfnn_conv_layer_pure_2d_SO_avg(input, basis, omaps, is_training, bn_mom, rn_mom, strides=[1, 1, 1, 1], padding='SAME'):
 	with tf.device('/cpu:0'):
@@ -437,7 +442,7 @@ def _rfnn_conv_layer_pure_2d_SO_avg(input, basis, omaps, is_training, bn_mom, rn
 	scales = np.shape(basis)[0]
 	orientations = np.shape(basis)[1]
 
-#	kernels = []
+	kernels = []
 	outputs = []
 	for i in range(scales):
 		for j in range(orientations):
@@ -448,14 +453,20 @@ def _rfnn_conv_layer_pure_2d_SO_avg(input, basis, omaps, is_training, bn_mom, rn
 			kernel = kernel[0,0,:,:,:,:]
 			conv = tf.nn.conv2d(input, kernel, strides=strides, padding=padding)
 
-#			kernels.append(kernel)
+			kernels.append(kernel)
 			outputs.append(conv)
 
 	outputs = tf.stack(outputs)
 	output = tf.reduce_mean(outputs, reduction_indices=[0])
 #	kernel = tf.reduce_mean(kernels, reduction_indices=[0])
 
-	return output, alphas
+	kernels = tf.stack(kernels)
+	kernels = tf.transpose(kernels, (1,2,3,4,0))
+	kernels = tf.reshape(kernels, (kernels.get_shape()[0].value,kernels.get_shape()[1].value,
+								   kernels.get_shape()[2].value,
+								   kernels.get_shape()[3].value*kernels.get_shape()[4].value))
+
+	return output, alphas, kernels
 
 def _rfnn_conv_layer_pure_2d_SO_learn_sq_bc(input, basis, omaps, is_training, bn_mom, rn_mom, strides=[1, 1, 1, 1], padding='SAME'):
 	with tf.device('/cpu:0'):
@@ -469,6 +480,7 @@ def _rfnn_conv_layer_pure_2d_SO_learn_sq_bc(input, basis, omaps, is_training, bn
 	orientations = np.shape(basis)[1]
 
 	outputs = []
+	kernels = []
 	for i in range(scales):
 		for j in range(orientations):
 			kernel = tf.reduce_sum(
@@ -476,6 +488,8 @@ def _rfnn_conv_layer_pure_2d_SO_learn_sq_bc(input, basis, omaps, is_training, bn
 				, axis=2, name='weights_' + str(i))
 
 			kernel = kernel[0,0,:,:,:,:]
+			kernels.append(kernel)
+
 			conv = tf.nn.conv2d(input, kernel, strides=strides, padding=padding)
 
 			outputs.append(conv)
@@ -512,7 +526,13 @@ def _rfnn_conv_layer_pure_2d_SO_learn_sq_bc(input, basis, omaps, is_training, bn
 	#
 	#			tf.summary.image('filters', kernel_transposed, max_outputs=batch)
 
-	return output, alphas
+	kernels = tf.stack(kernels)
+	kernels = tf.transpose(kernels, (1,2,3,4,0))
+	kernels = tf.reshape(kernels, (kernels.get_shape()[0].value,kernels.get_shape()[1].value,
+								   kernels.get_shape()[2].value,
+								   kernels.get_shape()[3].value*kernels.get_shape()[4].value))
+
+	return output, alphas, kernels
 
 def _rfnn_conv_layer_pure_2d_SO_learn_fl_bc(input, basis, omaps, strides=[1, 1, 1, 1], padding='SAME'):
 	with tf.device('/cpu:0'):
@@ -562,10 +582,8 @@ def _rfnn_conv_layer_pure_2d_SO_learn_fl_bc(input, basis, omaps, strides=[1, 1, 
 
 	return output, alphas, kernel
 
-
-
 # === altered output size, omaps*S*O ===
-def _rfnn_conv_layer_pure_2d_SO_learn_flatten(input, basis, omaps, strides=[1, 1, 1, 1], padding='SAME'):
+def _rfnn_conv_layer_pure_2d_SO_learn_flatten(input, basis, omaps, is_training, bn_mom, rn_mom, strides=[1, 1, 1, 1], padding='SAME'):
 	# flattening all scales and orientation -> output size = omaps*scales*orientations
 	# number of alphas stays omaps
 	with tf.device('/cpu:0'):
@@ -584,7 +602,7 @@ def _rfnn_conv_layer_pure_2d_SO_learn_flatten(input, basis, omaps, strides=[1, 1
 								 kernel.get_shape()[3].value*kernel.get_shape()[4].value*kernel.get_shape()[5].value))
 
 	output = tf.nn.conv2d(input, kernel, strides=strides, padding=padding)
-	print(output.get_shape())
+#	print(output.get_shape())
 	#	print(merged_outputs.get_shape())
 
 	#		with tf.variable_scope('sigma%d' % i):
@@ -873,7 +891,7 @@ def init_basis_hermite_steerable_2D(kernel, sigmas, theta=90.0, order=4):
 																							  g1, axis=1) \
 				+ np.power(np.cos(np.pi / 2.0 - t * angle), 4) * conv(gauss0x, g4, axis=0)
 
-		hermiteBasis[i, :, :, :, :] /= sigma
+		hermiteBasis[i, :, :, :, :] /= np.sqrt(sigma)
 
 	with tf.device('/cpu:0'):
 		return tf.constant(hermiteBasis[:, :, 0:order + 1, :, :], dtype=tf.float32)
@@ -948,7 +966,7 @@ def init_basis_hermite_steerable_full(kernel, sigmas, theta=90.0, order=4):
 			hermiteBasis[i, t, 8, :, :] = \
 				rotate(conv(conv(impulse, g2, axis=1), g2, axis=0), -t * theta, reshape=False)
 
-		hermiteBasis[i, :, :, :, :] /= sigma
+		hermiteBasis[i, :, :, :, :] /= np.sqrt(sigma)
 
 	with tf.device('/cpu:0'):
 		return tf.constant(hermiteBasis[:, :, 0:threshold, :, :], dtype=tf.float32)

Training/Utils/training_utils.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
 
 def get_experiment_infos(FLAGS):
 	l = [
@@ -41,13 +42,14 @@ def get_kernels(i):
 	return alphas, kernel_transposed
 
 def show_kernels(kernels):
-	f, axarr = plt.subplots(8, 8)
-	f.set_figheight(12)
-	f.set_figwidth(12)
+	k = int(np.sqrt(np.shape(kernels)[-1]))
+	f, axarr = plt.subplots(k, k)
+	f.set_figheight(2*k)
+	f.set_figwidth(2*k)
 	f.subplots_adjust(hspace=0.05, wspace=0.05)
-	for i in range(8):
-		for j in range(8):
-			axarr[j, i].imshow(kernels[i * 3 + j], cmap='gray')
+	for i in range(k):
+		for j in range(k):
+			axarr[j, i].imshow(kernels[:,:,i * k + j], cmap='gray')
 			axarr[j, i].set_axis_off()
 
 def exp_GB(logits, alpha):
@@ -64,7 +66,7 @@ def tower_accuracy(logits, labels, scope):
 		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 	#        tf.summary.scalar('Accuracy', accuracy)
 
-	return accuracy
+	return accuracy, correct_prediction, softmax
 
 def tower_accuracy_exp(logits, labels, scope):
 	#    softmax = tf.nn.softmax(logits)

Training/run_models.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import shutil
 from train_cifar import train_cifar
 from train_model import train_ctnet
+from train_model_cnn import train_ctnet_cnn
 
 NUM_GPUS = 1
 
@@ -19,9 +20,9 @@ def initialize_folders():
     """
 	if not tf.gfile.Exists(FLAGS.log_dir):
 		tf.gfile.MakeDirs(FLAGS.log_dir)
-	else:
-		shutil.rmtree(FLAGS.log_dir, ignore_errors=True)
-		tf.gfile.MakeDirs(FLAGS.log_dir)
+#	else:
+#		shutil.rmtree(FLAGS.log_dir, ignore_errors=True)
+#		tf.gfile.MakeDirs(FLAGS.log_dir)
 
 #	if not tf.gfile.Exists(FLAGS.checkpoint_dir):
 #		tf.gfile.MakeDirs(FLAGS.checkpoint_dir)
@@ -45,18 +46,40 @@ def str2bool(s):
 FLAGS = FLAGS()
 
 # Training
-FLAGS.weight_decay = 1e-4
-FLAGS.nesterov_momentum = 0.9
 FLAGS.pretraining = False
+FLAGS.max_epochs = 100
+FLAGS.batch_size = 32
+FLAGS.xvalidation_folds = 4
+FLAGS.trials = 3
+# Adam
+FLAGS.beta1 = 0.9
+FLAGS.beta2 = 0.999
+FLAGS.epsilon = 1e-3
+
+# LSUV
+FLAGS.t_max = 20
+FLAGS.t_min = 10
+FLAGS.tol_var = 0.01
 
 # Data
 #FLAGS.cifar_path = '/home/hilbysfe/dev/DNN-for-CT-Classification/Training/cifar-10-batches-py'
 #FLAGS.cifar_path = r'D:\Adam Hilbert\CT_Classification\code\Training\cifar10\cifar-10-batches-py'
 FLAGS.normalization = True
-FLAGS.datapath = r'D:\Adam Hilbert\Data\data_binaries\MIP2D\affected_side'
+FLAGS.X_dim = 433
+#	FLAGS.X_dim = 336
+#	FLAGS.Z_dim = 252
 
 # General CNN
 FLAGS.bases3d = False
+FLAGS.init_kernel = 11
+FLAGS.comp_kernel = 5
+FLAGS.init_order = 3
+FLAGS.comp_order = 3
+FLAGS.bnorm_mom = 0.7
+FLAGS.renorm = 0.7
+FLAGS.beta_wd = 1e-4
+
+FLAGS.keep_prob = 0.9
 
 # DenseNet
 FLAGS.total_blocks = 4
@@ -65,65 +88,45 @@ FLAGS.model_type = 'DenseNet'
 FLAGS.bc_mode = True
 
 # Logging
-FLAGS.print_freq = 1
+FLAGS.print_freq = 3
 FLAGS.eval_freq = 1
-FLAGS.checkpoint_freq = 0
 
 configs = [
-
-	# Adam
-#	[7, 3, 0.0002, 24, 0.8, 37, 150, 32, 150, 150, '2.0,0.5', 3, 3, "single", 0.7, 0.0, False, 0.95, 0.999, 1e-3, 0.7],
-#	[7, 3, 0.0002, 24, 0.8, 37, 150, 32, 150, 150, '2.0,0.5', 3, 3, "single", 0.7, 0.0, False, 0.95, 0.999, 1e-2, 0.7],
-#	[7, 3, 0.0002, 24, 0.8, 37, 150, 32, 150, 150, '2.0,0.5', 3, 3, "single", 0.7, 0.0, False, 0.95, 0.999, 0.1, 0.7],
-#	[7, 3, 0.0002, 24, 0.8, 37, 150, 32, 150, 150, '2.0,0.5', 3, 3, "single", 0.7, 0.0, False, 0.95, 0.999, 1.0, 0.7],
-#	[7, 3, 0.0002, 24, 0.8, 37, 150, 32, 150, 150, '2.0,0.5', 3, 3, "single", 0.7, 1e-6, False, 0.95, 0.999, 1e-4, 0.7],
-
-	[7, 3, 0.0003, 6, 0.8, 37, 200, 32, 200, 200, '2.0,1.5', '1.0,0.5', 3, 3, "avg", 0.7, False, 0.9, 0.999, 1e-3, 0.7, 1e-4, '30, 30'],
-#	[7, 3, 0.0003, 6, 0.8, 37, 150, 32, 150, 150, '2.0,1.5', '1.0,0.5', 3, 3, "max", 0.7, False, 0.9, 0.999, 1e-3, 0.7, 1e-4, '30, 30'],
-	[7, 3, 0.0003, 8, 0.8, 37, 150, 32, 150, 150, '2.0,1.5', '1.0,0.5', 3, 3, "avg", 0.7, False, 0.9, 0.999, 1e-3, 0.7, 1e-4, '45, 45'],
-	[7, 3, 0.0003, 8, 0.8, 37, 150, 32, 150, 150, '2.0,1.5', '1.0,0.5', 3, 3, "max", 0.7, False, 0.9, 0.999, 1e-3, 0.7, 1e-4, '45, 45'],
+#	SHAllOW
+#	[0.0003, 8, 21, 200, 200, '2.0', '0.5', "single", False, '45, 45', 'nihss_imp'],
+#	[0.0003, 8, 21, 200, 200, '2.0', '0.5', "single", False, '45, 45', 'tici_imp'],
+#	[0.0003, 8, 21, 200, 200, '2.0', '0.5', "single", False, '45, 45', 'mrs'],
+	[0.0003, 8, 21, 200, 200, '2.0,1.5', '1.0,0.5', "learn_sq", False, '45, 45', 'collaterals_imp'],
+	[0.0003, 8, 21, 200, 200, '2.0,1.5', '1.0,0.5', "learn_sq", False, '45, 45', 'nihss_imp'],
+	[0.0003, 8, 21, 200, 200, '2.0,1.5', '1.0,0.5', "learn_sq", False, '45, 45', 'tici_imp'],
+	[0.0003, 8, 21, 200, 200, '2.0,1.5', '1.0,0.5', "learn_sq", False, '45, 45', 'mrs'],
+	[0.0003, 8, 21, 200, 200, '2.0,1.5', '1.0,0.5', "learn_sq", False, '45, 45', 'affected_side'],
+#	DEEP
+#	[0.0003, 8, 37, 200, 200, '2.0,1.5', '1.0,0.5', "learn_sq", False, '45, 45', 'affected_side'],
+#	[0.0003, 8, 37, 200, 200, '2.0,1.5', '1.0,0.5', "avg", False, '45, 45', 'collaterals_imp'],
+#	[0.0003, 8, 37, 200, 200, '2.0,1.5', '1.0,0.5', "single", False, '45, 45', 'nihss_imp'],
+#	[0.0003, 8, 37, 200, 200, '2.0,1.5', '1.0,0.5', "learn_sq", False, '45, 45', 'tici_imp'],
+#	[0.0003, 8, 37, 200, 200, '2.0,1.5', '1.0,0.5', "avg", False, '45, 45', 'mrs'],
+#	[0.0003, 8, 37, 200, 200, '2.0,1.5', '1.0,0.5', "max", False, '45, 45']
 ]
 
+
 for config in configs:
-	FLAGS.init_kernel = config[0]
-	FLAGS.comp_kernel = config[1]
-	FLAGS.learning_rate = config[2]
-	FLAGS.growth_rate = config[3]
-	FLAGS.keep_prob = config[4]
-	FLAGS.depth = config[5]
-	FLAGS.max_epochs = config[6]
-	FLAGS.batch_size = config[7]
-	FLAGS.reduce_lr_epoch_1 = config[8]
-	FLAGS.reduce_lr_epoch_2 = config[9]
-	FLAGS.init_sigmas = config[10]
-	FLAGS.comp_sigmas = config[11]
-	FLAGS.init_order = config[12]
-	FLAGS.comp_order = config[13]
-	FLAGS.rfnn = config[14]
-	FLAGS.bnorm_mom = config[15]
-	FLAGS.bnorm_inc = config[16]
-	FLAGS.beta1 = config[17]
-	FLAGS.beta2 = config[18]
-	FLAGS.epsilon = config[19]
-	FLAGS.renorm = config[20]
-	FLAGS.beta_wd = config[21]
-	FLAGS.thetas = config[22]
-	FLAGS.tol_var = 0.01
-	FLAGS.t_max = 20
-	FLAGS.t_min = 10
-	FLAGS.X_dim = 433
-#	FLAGS.X_dim = 336
-#	FLAGS.Z_dim = 252
+	FLAGS.datapath = r'D:\Adam Hilbert\Data\data_binaries\MIP2D\\'
 
-	FLAGS.xvalidation_folds = 4
+	FLAGS.learning_rate = config[0]
+	FLAGS.growth_rate = config[1]
+	FLAGS.depth = config[2]
+	FLAGS.reduce_lr_epoch_1 = config[3]
+	FLAGS.reduce_lr_epoch_2 = config[4]
+	FLAGS.init_sigmas = config[5]
+	FLAGS.comp_sigmas = config[6]
+	FLAGS.rfnn = config[7]
+	FLAGS.bnorm_inc = config[8]
+	FLAGS.thetas = config[9]
+	FLAGS.datapath = FLAGS.datapath + config[10]
 
-	#    FLAGS.log_dir = '/home/nicolab/DATA/logs/NCCT_CTA/DenseNet/bests/'
-#	FLAGS.log_dir = r'/home/hilbysfe/dev/DNN-for-CT-Classification/Training/logs/MIP2D/standard/' \
-#	FLAGS.log_dir = r'D:\Adam Hilbert\CT_Classification\code\Training\logs\NCCT\standard\\' \
-#	FLAGS.log_dir = r'D:\Adam Hilbert\CT_Classification\code\Training\logs\MIP2D\rfnn\single_scale\Adam\lsuv\\' \
-
-#	TESTING NEW DATASET
-	FLAGS.log_dir = r'E:\logs\MIP2D_affected\rfnn\SO\test\\' \
+	FLAGS.log_dir = r'D:\Experiments\logs\MIP2D_' + config[10] + '\\rfnn\Shallow\\' \
 					+ str(FLAGS.init_kernel) + 'x' + str(FLAGS.comp_kernel) + '_' \
 					+ str(FLAGS.learning_rate) + '_' \
 					+ str(FLAGS.growth_rate) + '_' \
@@ -140,22 +143,51 @@ for config in configs:
 					+ str(FLAGS.beta1) + '-' + str(FLAGS.beta2) + '-' + str(FLAGS.epsilon) + '_red-' \
 					+ str(FLAGS.reduction) + 'adaptWD-nobeta_' \
 					+ str(FLAGS.thetas)
-#					+ str(FLAGS.beta_wd) + '_' + '-wd' + str(FLAGS.beta_wd) + '_' \
-
-#					+ str(FLAGS.nesterov_momentum) \
-#					+ str(FLAGS.init_order) + '_' \
-#					+ str(FLAGS.comp_order) + '_' \
+	#					+ str(FLAGS.beta_wd) + '_' + '-wd' + str(FLAGS.beta_wd) + '_' \
 	if FLAGS.bnorm_inc:
 		FLAGS.log_dir += '_bnorm_inc' + '-0.75'
-	#    FLAGS.checkpoint_dir = '/home/nicolab/DATA/checkpoints/NCCT_CTA/DenseNet/' \
-#	FLAGS.checkpoint_dir = 'D:\Adam Hilbert\CT_Classification\code\Training\checkpoints\\' \
-#						   + str(FLAGS.init_kernel) + 'x' + str(FLAGS.comp_kernel) + '_' \
-#						   + str(FLAGS.learning_rate) + '_' \
-#						   + str(FLAGS.growth_rate) + '_' \
-#						   + str(FLAGS.depth) + '_' \
-#						   + str(FLAGS.total_blocks) + '_' \
-#						   + str(FLAGS.keep_prob) + '_' \
-#						   + str(FLAGS.reduction)
+
+	FLAGS.checkpoint_dir = r'D:\Experiments\checkpoints\MIP2D_' + config[10] + '\\rfnn\Shallow\\' \
+						   + str(FLAGS.init_kernel) + 'x' + str(FLAGS.comp_kernel) + '_' \
+						   + str(FLAGS.learning_rate) + '_' \
+						   + str(FLAGS.growth_rate) + '_' \
+						   + str(FLAGS.depth) + '_' \
+						   + str(FLAGS.total_blocks) + '_' \
+						   + str(FLAGS.keep_prob) + '_' \
+						   + str(FLAGS.batch_size) + '_' \
+						   + str(FLAGS.rfnn) + '_' \
+						   + str(FLAGS.init_sigmas) + str(FLAGS.comp_sigmas) + '_' \
+						   + str(FLAGS.reduce_lr_epoch_1) + '_' \
+						   + str(FLAGS.reduce_lr_epoch_2) + '_' + str(FLAGS.max_epochs) + '_' \
+						   + str(FLAGS.bnorm_mom) \
+						   + str(FLAGS.renorm) + '_' \
+						   + str(FLAGS.beta1) + '-' + str(FLAGS.beta2) + '-' + str(FLAGS.epsilon) + '_red-' \
+						   + str(FLAGS.reduction) + 'adaptWD-nobeta_' \
+						   + str(FLAGS.thetas)
+	#					+ str(FLAGS.beta_wd) + '_' + '-wd' + str(FLAGS.beta_wd) + '_' \
+	if FLAGS.bnorm_inc:
+		FLAGS.checkpoint_dir += '_bnorm_inc' + '-0.75'
+
+	FLAGS.stat_dir = r'D:\Experiments\stats\MIP2D_' + config[10] + '\\rfnn\Shallow\\' \
+					 + str(FLAGS.init_kernel) + 'x' + str(FLAGS.comp_kernel) + '_' \
+					 + str(FLAGS.learning_rate) + '_' \
+					 + str(FLAGS.growth_rate) + '_' \
+					 + str(FLAGS.depth) + '_' \
+					 + str(FLAGS.total_blocks) + '_' \
+					 + str(FLAGS.keep_prob) + '_' \
+					 + str(FLAGS.batch_size) + '_' \
+					 + str(FLAGS.rfnn) + '_' \
+					 + str(FLAGS.init_sigmas) + str(FLAGS.comp_sigmas) + '_' \
+					 + str(FLAGS.reduce_lr_epoch_1) + '_' \
+					 + str(FLAGS.reduce_lr_epoch_2) + '_' + str(FLAGS.max_epochs) + '_' \
+					 + str(FLAGS.bnorm_mom) \
+					 + str(FLAGS.renorm) + '_' \
+					 + str(FLAGS.beta1) + '-' + str(FLAGS.beta2) + '-' + str(FLAGS.epsilon) + '_red-' \
+					 + str(FLAGS.reduction) + 'adaptWD-nobeta_' \
+					 + str(FLAGS.thetas)
+	#					+ str(FLAGS.beta_wd) + '_' + '-wd' + str(FLAGS.beta_wd) + '_' \
+	if FLAGS.bnorm_inc:
+		FLAGS.stat_dir += '_bnorm_inc' + '-0.75'
 
 	initialize_folders()
 	train_ctnet(FLAGS, NUM_GPUS)